83c5c5f40c13432e4fdd475bf770aa7510589701
Corey Burns <coreyburnsdev@aol.com>
2026-02-03 10:12:54 -0500

    Improve code blocks and add lint-staged

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,16 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	markdown: {
+		syntaxHighlight: "shiki",
+		shikiConfig: {
+			themes: {
+				light: "one-dark-pro",
+				dark: "material-theme-palenight",
+			},
+			wrap: true,
+		},
+	},
 	vite: {
 		plugins: [tailwind()],
 	},
