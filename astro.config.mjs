6900592960bb0fb446002bebcead9154ff3fe4bc
Corey Burns <coreyburnsdev@aol.com>
2026-02-03 09:25:50 -0500

    Save workspace changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
+import tailwind from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  // ...
+	vite: {
+		plugins: [tailwind()],
+	},
 });
